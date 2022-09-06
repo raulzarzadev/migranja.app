aa755b5bc20392106b7135a5b0f53b34763af549
raulzarza <raulzarza.dev@gmail.com>
2022-09-06 16:15:05 -0600

    redux store and golbal state setted

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,15 @@
 import type { NextPage } from "next";
 import Head from "next/head";
+import { useDispatch, useSelector } from "react-redux";
 import Layout from "../components/layout";
+import { selectAuthState, setAuthState } from "../slices/authSlice";
+import { decrement, increment, selectCountState } from "../slices/couterSlice";
 
 const Home: NextPage = () => {
+  const dispatch = useDispatch();
+  const authState = useSelector(selectAuthState);
+  const counterValue = useSelector(selectCountState);
+
   return (
     <div>
       <Head>
@@ -12,6 +19,40 @@ const Home: NextPage = () => {
       </Head>
       <Layout>
         <div>Hola, bienvenido al tempalte de nextjs pwa tailwind</div>
+        <div>
+          <h1>Auth state</h1>
+          <div className="flex w-full justify-center flex-col items-center">
+            <div>{authState ? "Logged in" : "Not Logged In"}</div>
+            <button
+              className="bg-slate-500 p-2 rounded-lg  border-2 hover:border-transparent"
+              onClick={() =>
+                authState
+                  ? dispatch(setAuthState(false))
+                  : dispatch(setAuthState(true))
+              }
+            >
+              {authState ? "Logout" : "LogIn"}
+            </button>
+          </div>
+        </div>
+        <div>
+          <h1>Couter state</h1>
+          <div className="flex w-full justify-center flex-col items-center">
+            <div>The counter value is : {counterValue}</div>
+            <button
+              className=" w-full h-10 bg-green-400/50"
+              onClick={() => dispatch(increment())}
+            >
+              Increment
+            </button>
+            <button
+              className=" w-full h-10 bg-red-400/50"
+              onClick={() => dispatch(decrement())}
+            >
+              Decrement
+            </button>
+          </div>
+        </div>
       </Layout>
     </div>
   );
