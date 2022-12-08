e21d83f9dfb66b42a054850a3ec4a38bef144ca1
Raul Zarza <raulzarza.dev@gmail.com>
2022-12-07 22:31:58 -0700

    add firebase, update gitignore

---
+++ firebase/main.ts
@@ -0,0 +1,12 @@
+import { initializeApp } from 'firebase/app'
+import { getAuth } from 'firebase/auth'
+import { getFirestore } from 'firebase/firestore'
+import { getStorage } from 'firebase/storage'
+
+const firebaseConfig = process.env.NEXT_PUBLIC_FIREBASE_CONFIG ?? ''
+
+const app = initializeApp(JSON.parse(firebaseConfig))
+export const db = getFirestore(app)
+
+export const auth = getAuth(app)
+export const storage = getStorage(app)
