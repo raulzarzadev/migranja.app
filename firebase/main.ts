976fea871d8c6640a23c0ed5a0aba2a06e901d3b
Raul Zarza <raulzarza.dev@gmail.com>
2022-12-14 02:44:31 -0700

    enable firebase offline persisntence

--- firebase/main.ts
+++ firebase/main.ts
@@ -1,7 +1,21 @@
 import { initializeApp } from 'firebase/app'
-import { getFirestore } from 'firebase/firestore'
+import { enableIndexedDbPersistence, getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = process.env.NEXT_PUBLIC_FIREBASE_CONFIG ?? ''
 
 export const app = initializeApp(JSON.parse(firebaseConfig))
 export const db = getFirestore(app)
+
+enableIndexedDbPersistence(db)
+  .then(() => console.log('Enabled offline persistence'))
+  .catch((error) => {
+    if (error.code == 'failed-precondition') {
+      // Multiple tabs open, persistence can only be enabled
+      // in one tab at a a time.
+      // ...
+    } else if (error.code == 'unimplemented') {
+      // The current browser does not support all of the
+      // features required to enable persistence
+      // ...
+    }
+  })
