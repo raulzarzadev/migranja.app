3daa548eb9bffa6611dbb470a6ede7528c9d6008
Raul Zarza <raulzarza.dev@gmail.com>
2023-01-14 10:51:51 -0700

    fix debugging logs

--- next.config.js
+++ next.config.js
@@ -1,14 +1,23 @@
 /** @type {import('next').NextConfig} */
+const runtimeCaching = require('next-pwa/cache')
+
 const withPWA = require('next-pwa')({
   dest: 'public'
   // put other next-pwa options here
 })
 
 const nextConfig = withPWA({
+  pwa: {
+    dest: 'public',
+    scope: '/',
+    runtimeCaching,
+    disable: process.env.NODE_ENV === 'development'
+  },
   reactStrictMode: true,
   images: {
     domains: ['lh3.googleusercontent.com']
   }
+
   // put other next js options here
 })
 
