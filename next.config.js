d9a17e5e8be7b5b01d699ea4fa9e39cb8f7be825
Raul Zarza <raulzarza.dev@gmail.com>
2023-01-15 10:21:22 -0700

    modify next.config

--- next.config.js
+++ next.config.js
@@ -2,10 +2,10 @@
 const runtimeCaching = require('next-pwa/cache')
 
 const withPWA = require('next-pwa')({
-  dest: 'public',
+  dest: 'public'
   // disable: process.env.NODE_ENV === 'development',
   // register: true,
-  sw: 'sw.js'
+  // sw: 'sw.js'
   // put other next-pwa options here
 })
 
