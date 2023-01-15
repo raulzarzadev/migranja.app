fe39fbdecf232427c05fbbaf38d5fa4b485dfcaf
Raul Zarza <raulzarza.dev@gmail.com>
2023-01-15 09:57:20 -0700

    updateSW

--- next.config.js
+++ next.config.js
@@ -2,17 +2,14 @@
 const runtimeCaching = require('next-pwa/cache')
 
 const withPWA = require('next-pwa')({
-  dest: 'public'
+  dest: 'public',
+  // disable: process.env.NODE_ENV === 'development',
+  // register: true,
+  sw: 'sw.js'
   // put other next-pwa options here
 })
 
 const nextConfig = withPWA({
-  // pwa: {
-  //   dest: 'public',
-  //   scope: '/',
-  //   disable: process.env.NODE_ENV === 'development'
-  //   runtimeCaching,
-  // },
   reactStrictMode: true,
   images: {
     domains: ['lh3.googleusercontent.com']
