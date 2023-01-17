b9cba8c96be24d3194de3cbe4e49ce8bea452724
Raul Zarza <raulzarza.dev@gmail.com>
2023-01-16 20:31:01 -0700

    ahora puedes abrir un modal con los detalles del animal al que le des click en la lista de destetes

--- components/FarmEvents/EventsList.tsx
+++ components/FarmEvents/EventsList.tsx
@@ -3,6 +3,10 @@ import DebouncedInput from 'components/inputs/DebouncedInput'
 import { useEffect, useState } from 'react'
 import { FarmState, FarmStateAnimalEvent } from 'store/slices/farmSlice'
 import { AnimalWeaning } from 'types/base/AnimalWeaning.model'
+import {
+  animalCurrentStatusLabels,
+  AnimalCurrentStatusType
+} from 'types/base/LABELS_TYPES/AnimalCurrentStatus'
 import { labelsOfFarmEventTypes } from 'types/base/LABELS_TYPES/EventTypes'
 import FarmEventCard from './FarmEvent/FarmEventCard'
 
@@ -16,28 +20,14 @@ export const EventsList = ({ events }: { events: FarmState['events'] }) => {
       setFilteredEvents([...arraySorted])
     } else {
       const filtered = [...arraySorted].filter((event) => {
-        const dictionary: Record<
-          FarmStateAnimalEvent['type'] | AnimalWeaning['type'],
-          string
-        > = {
-          BREEDING: 'Monta',
-          REMOVE: 'Removida',
-          BIRTH: 'Parto',
-          ABORT: 'Aborto',
-          EMPTY: 'Vacio',
-          DROP_OUT: 'Baja',
-          DROP_IN: 'Alta',
-          WEANING: 'Destete',
-          PENDING: 'Pendiente'
-        }
         return (
-          dictionary[event?.type]
+          animalCurrentStatusLabels[event?.type as AnimalCurrentStatusType]
             .toLowerCase()
             .includes(filterBy.toLowerCase()) ||
-          event?.eventData.breedingBatch?.find(({ earring }) =>
+          event?.eventData.breedingBatch?.find(({ earring }: any) =>
             earring?.includes(filterBy)
           ) ||
-          event?.eventData.calfs?.find(({ earring }) =>
+          event?.eventData.calfs?.find(({ earring }: any) =>
             earring?.includes(filterBy)
           ) ||
           event?.eventData?.parents?.father?.earring?.includes(filterBy) ||
@@ -89,75 +79,4 @@ export const EventsList = ({ events }: { events: FarmState['events'] }) => {
       </div>
     </div>
   )
-}
-
-const SortedOptions = ({
-  options,
-  sortBy
-}: {
-  options: { value: string; label: string }[]
-  sortBy: (val: string) => void
-}) => {
-  return (
-    <div className="flex w-full justify-center">
-      {options?.map((option) => (
-        <button
-          key={option.value}
-          className={`btn btn-outline btn-sm`}
-          onClick={() => sortBy(option.value)}
-        >
-          {option.label}
-        </button>
-      ))}
-    </div>
-  )
-}
-
-const FilterOptions = ({
-  label,
-  options,
-  setOption
-}: {
-  label: string
-  options: { label: string; value: string }[]
-  setOption: (value: string) => void
-}) => {
-  return (
-    <div className="form-control">
-      <label htmlFor="filter" className="label">
-        <span className="text-label">{label}</span>
-      </label>
-      <select
-        onChange={({ target: { value } }) => setOption(value)}
-        id="filter"
-        className="input input-sm w-min input-bordered"
-      >
-        <option value={''}>Todos </option>
-        {options?.map(({ label, value }) => (
-          <option key={value} value={value}>
-            {label}
-          </option>
-        ))}
-      </select>
-    </div>
-  )
-}
-
-interface SelectOption {
-  label: string
-  value: string
-}
-
-const getEventTypesOptions = (events: FarmState['events']): SelectOption[] => {
-  const dictionary = labelsOfFarmEventTypes
-
-  const options: SelectOption[] = events.reduce(
-    (prev: SelectOption[], curr) => {
-      const notExist = prev.find((option) => option.value === curr?.type)
-      if (notExist) return prev
-      return [...prev, { label: dictionary[curr.type], value: curr.type }]
-    },
-    []
-  )
-  return options
 }
--- components/WeaningEvents/index.tsx
+++ components/WeaningEvents/index.tsx
@@ -1,10 +1,14 @@
+import { AnimalDetails } from '@comps/AnimalCard'
 import AnimalsTable from '@comps/AnimalsTable'
 import { WEANING_STATUS_LABELS } from '@comps/FarmEvents/FarmEvent/WeaningEventCard'
 import { IconStatus } from '@comps/IconBreedingStatus'
 import DebouncedInput from '@comps/inputs/DebouncedInput'
+import Modal from '@comps/modal'
 import ModalEditWeaning from '@comps/modal/ModalEditWeaning'
+import { useState } from 'react'
 import { useSelector } from 'react-redux'
 import { selectFarmAnimals, selectFarmEvents } from 'store/slices/farmSlice'
+import { AnimalType } from 'types/base/AnimalType.model'
 import { myFormatDate } from 'utils/dates/myDateUtils'
 import { defineStatusByDate } from 'utils/defineStatusByDate'
 
@@ -21,8 +25,28 @@ const WeaningEvents = () => {
       return 0
     })
 
+  const [openAnimal, setOpenAnimal] = useState(false)
+  const [animal, setAnimal] = useState<AnimalType | undefined>(undefined)
+  const handleOpenAnimal = (earring?: string) => {
+    setOpenAnimal(!openAnimal)
+    if (earring) {
+      setAnimal(farmAnimals.find((animal) => animal.earring === earring))
+    } else {
+      setAnimal(undefined)
+    }
+  }
+
   return (
     <div className="w-full p-2">
+      {animal && (
+        <Modal
+          title="Detalles de animal"
+          open={openAnimal}
+          handleOpen={() => handleOpenAnimal()}
+        >
+          <AnimalDetails animal={animal} />
+        </Modal>
+      )}
       <div>Pendientes: {weaning.length}</div>
       {/* <AnimalsTable
         animalsData={weaning.map((event) =>
@@ -52,7 +76,15 @@ const WeaningEvents = () => {
                 key={event.id}
                 className="bg-base-300 rounded-md my-2 p-1 px-2"
               >
-                <td>{event?.eventData?.earring}</td>
+                <td>
+                  <button
+                    onClick={(e) => {
+                      handleOpenAnimal(event?.eventData.earring)
+                    }}
+                  >
+                    {event?.eventData?.earring}
+                  </button>
+                </td>
                 <td>{myFormatDate(event?.eventData?.date, 'dd MMM yy')}</td>
 
                 <td>
