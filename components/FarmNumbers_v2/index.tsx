6d7ae1cdac0a61b70dd77d933c9660624b97d48e
Raul Zarza <raulzarza.dev@gmail.com>
2023-02-20 21:01:24 -0700

    ahora puedes ver la nueva lista eventos

--- components/FarmNumbers_v2/index.tsx
+++ components/FarmNumbers_v2/index.tsx
@@ -18,6 +18,10 @@ const FarmNumbers = () => {
     ({ gender }) => gender === 'female'
   )
   const maleAnimals = activeAnimals.filter(({ gender }) => gender === 'male')
+  const activeFemales = activeAnimals.filter(
+    ({ gender }) => gender === 'female'
+  )
+  const activeMales = activeAnimals.filter(({ gender }) => gender === 'male')
 
   //* Animals by state
 
@@ -52,161 +56,162 @@ const FarmNumbers = () => {
     Hembras: [
       {
         title: 'Libres',
-        subTitle: 'Sin montas ni amamando',
-        animals: femaleAnimals.filter(({ state }) => state === 'FREE')
+        subTitle: 'En edad pero sin compromisos',
+        animals: activeFemales.filter(({ state }) => state === 'FREE')
       },
       {
         title: 'En monta',
         subTitle: 'Preñadas o no',
-        animals: femaleAnimals.filter(({ state }) => state === 'BREEDING')
+        animals: activeFemales.filter(({ state }) => state === 'BREEDING')
       },
       {
         title: 'Amamantando',
         subTitle: 'Recien paridas',
-        animals: femaleAnimals.filter(({ state }) => state === 'SUCKLE')
+        animals: activeFemales.filter(({ state }) => state === 'SUCKLE')
       },
       {
-        title: 'Lactando',
+        title: 'Lactandes',
         subTitle: 'Aun mamando',
         animals: femaleAnimals.filter(({ state }) => state === 'LACTATING')
       },
       {
-        title: 'En Monta',
-        subTitle: 'En Monta',
-        animals: femaleAnimals.filter(({ state }) => state === 'BREEDING')
-      },
-      {
         title: 'Corderas ',
-        subTitle: 'Para vientre. Destetadas y antes de monta',
-        animals: femaleAnimals.filter(({ state }) => state === 'FOR_BELLY')
+        subTitle: 'Para monta pero muy jovenes',
+        animals: activeFemales.filter(({ state }) => state === 'FOR_BELLY')
       },
       {
-        title: 'Para venta ',
-        subTitle: 'Para venta',
-        animals: femaleAnimals.filter(({ state }) => state === 'FOR_SALE')
+        title: 'Para venta',
+        subTitle: 'Listas para venderse',
+        animals: activeFemales.filter(({ state }) => state === 'FOR_SALE')
       },
       {
         title: 'Muertas',
         subTitle: 'Hembras muertas',
-        animals: femaleAnimals.filter(({ state }) => state === 'DEAD')
+        animals: activeFemales.filter(({ state }) => state === 'DEAD')
       },
       {
         title: 'Vendidas',
         subTitle: 'Hembras vendidas',
-        animals: femaleAnimals.filter(({ state }) => state === 'SOLD')
+        animals: activeFemales.filter(({ state }) => state === 'SOLD')
       }
     ],
     Machos: [
       {
         title: 'Engorda',
         subTitle: 'En engorda',
-        animals: maleAnimals.filter(({ state }) => state === 'FATTEN')
+        animals: activeMales.filter(({ state }) => state === 'FATTEN')
       },
       {
-        title: 'Lactando',
+        title: 'Lactandes',
         subTitle: 'Aun mamando',
-        animals: maleAnimals.filter(({ state }) => state === 'LACTATING')
+        animals: activeMales.filter(({ state }) => state === 'LACTATING')
       },
       {
         title: 'En venta',
         subTitle: 'Listos para venta',
-        animals: maleAnimals.filter(({ state }) => state === 'FOR_SALE')
+        animals: activeMales.filter(({ state }) => state === 'FOR_SALE')
       },
 
       {
         title: 'Muertos',
         subTitle: 'Muertos ',
-        animals: maleAnimals.filter(({ state }) => state === 'DEAD')
+        animals: activeMales.filter(({ state }) => state === 'DEAD')
       },
       {
         title: 'Vendidos',
         subTitle: 'Vendidos',
-        animals: maleAnimals.filter(({ state }) => state === 'SOLD')
+        animals: activeMales.filter(({ state }) => state === 'SOLD')
       }
     ]
   }
+
   const animalsByAge: Record<string, NumberRow[]> = {
     Total: [
       {
         title: 'Todos ',
-        subTitle: '',
+        subTitle: 'Todas las borregas',
         animals: activeAnimals
       },
       {
         title: '0-70 días',
-        subTitle: '',
+        subTitle: 'menores de 70 dias',
         animals: animalsBetweenDays(activeAnimals, 0, 70)
       },
       {
         title: '71-210 días',
-        subTitle: '',
+        subTitle: 'Entre 71-210 dias',
         animals: animalsBetweenDays(activeAnimals, 71, 210)
       },
       {
         title: '210-245 días',
-        subTitle: '',
+        subTitle: 'Entre 210 y 245 dias',
         animals: animalsBetweenDays(activeAnimals, 210, 245)
+      },
+      {
+        title: '+245 días',
+        subTitle: 'Mayores de 245 dias',
+        animals: animalsBetweenDays(activeAnimals, 245, 99999)
       }
     ],
     Hembras: [
       {
         title: 'Todas ',
         subTitle: 'Hembras activas',
-        animals: femaleAnimals
+        animals: activeFemales
       },
       {
         title: '0-70 días',
         subTitle: 'Hembras entre 0 y 70 días',
-        animals: animalsBetweenDays(femaleAnimals, 0, 70)
+        animals: animalsBetweenDays(activeFemales, 0, 70)
       },
       {
         title: '71-210 días',
         subTitle: 'Hembras entre 71 y 210 días',
-        animals: animalsBetweenDays(femaleAnimals, 71, 210)
+        animals: animalsBetweenDays(activeFemales, 71, 210)
       },
       {
         title: '210-245 días',
         subTitle: 'Hembras entre 210 y 245 días',
-        animals: animalsBetweenDays(femaleAnimals, 210, 245)
+        animals: animalsBetweenDays(activeFemales, 210, 245)
       },
 
       {
         title: '+245',
         subTitle: 'Mayores de 254 dias',
-        animals: animalsBetweenDays(femaleAnimals, 245, 9999)
+        animals: animalsBetweenDays(activeFemales, 245, 9999)
       }
     ],
     Machos: [
       {
         title: 'Todos ',
         subTitle: 'Machos activos ',
-        animals: maleAnimals
+        animals: activeMales
       },
       {
         title: 'Sementales',
         subTitle: 'Sementales activos',
-        animals: maleAnimals.filter((animal) => animal.isStallion)
+        animals: activeMales.filter((animal) => animal.isStallion)
       },
       {
         title: '0-70 días',
         subTitle: 'Machos activos entre 0-70 días',
-        animals: animalsBetweenDays(maleAnimals, 0, 70)
+        animals: animalsBetweenDays(activeMales, 0, 70)
       },
       {
         title: '71-210 días',
         subTitle: 'Machos activos entre 71-210 días',
-        animals: animalsBetweenDays(maleAnimals, 71, 210)
+        animals: animalsBetweenDays(activeMales, 71, 210)
       },
       {
         title: '210-245 días',
         subTitle: 'Machos activos entre 210 y 245 días',
-        animals: animalsBetweenDays(maleAnimals, 210, 245)
+        animals: animalsBetweenDays(activeMales, 210, 245)
       },
 
       {
         title: '+245',
         subTitle: 'Machos mayores de 254 dias',
-        animals: animalsBetweenDays(maleAnimals, 245, 9999)
+        animals: animalsBetweenDays(activeMales, 245, 9999)
       }
     ]
   }
