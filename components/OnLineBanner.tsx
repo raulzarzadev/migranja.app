e8e586f024f994c0bf390f3847b869298130905c
Raul Zarza <raulzarza.dev@gmail.com>
2023-05-22 08:56:10 -0700

    online banner sma for mobile view

--- components/OnLineBanner.tsx
+++ components/OnLineBanner.tsx
@@ -7,11 +7,13 @@ const OnLineBanner = () => {
     <div className="mx-2 ">
       {isOnline ? (
         <span className=" bg-success  text-white flex p-1 rounded-md shadow-md uppercase font-semibold px-2">
-          En Linea <Icon name="onLine" />
+          <span className="hidden sm:block">En Linea</span>{' '}
+          <Icon name="onLine" />
         </span>
       ) : (
         <span className="bg-error text-white flex p-1 rounded-md shadow-md uppercase font-semibold px-2">
-          Fuera de linea <Icon name="offLine" />
+          <span className="hidden sm:block">Fuera de linea</span>{' '}
+          <Icon name="offLine" />
         </span>
       )}
     </div>
