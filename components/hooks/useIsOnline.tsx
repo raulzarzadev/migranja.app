4ba688ba230d1128e13ef7c5d0e12d15a54e8d04
Raul Zarza <raulzarza.dev@gmail.com>
2023-05-22 08:20:36 -0700

    add cors config

--- components/hooks/useIsOnline.tsx
+++ components/hooks/useIsOnline.tsx
@@ -3,12 +3,11 @@ import { useEffect, useState } from 'react'
 const useIsOnline = () => {
   const [isOnline, setIsOnline] = useState<boolean | undefined>(undefined)
   useEffect(() => {
-    const url = 'https://api.adviceslip.com/advice'
+    const url = 'http://localhost:3000/api/hello'
     const fetchData = async () => {
       try {
         const response = await fetch(url)
-        const json = await response.json()
-        console.log(json)
+        console.log(response)
       } catch (error) {
         console.log('error', error)
       }
--- pages/api/hello.ts
+++ pages/api/hello.ts
@@ -1,13 +1,18 @@
 // Next.js API route support: https://nextjs.org/docs/api-routes/introduction
 import type { NextApiRequest, NextApiResponse } from 'next'
+import { cors, runMiddleware } from './cors'
 
 type Data = {
   isAlive: boolean
 }
 
-export default function handler(
+export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
+  // Run the middleware
+  await runMiddleware(req, res, cors)
+
+  // Rest of the API logic
   res.status(200).json({ isAlive: true })
 }
