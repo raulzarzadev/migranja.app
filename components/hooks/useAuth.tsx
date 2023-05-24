14512be604ca747f7a46fee77856a2ea1af6bbd5
Raul Zarza <raulzarza.dev@gmail.com>
2023-05-24 15:24:37 -0700

    fix types

--- components/hooks/useAuth.tsx
+++ components/hooks/useAuth.tsx
@@ -32,6 +32,7 @@ const useAuth = () => {
       }
       if (user) router.push('/home')
     })
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [auth, dispatch])
 
   return { user }
