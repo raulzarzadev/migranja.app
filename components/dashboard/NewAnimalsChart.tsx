91960b752a693ffe24384e42004b31d2dcec9d7e
Raul Zarza <raulzarza.dev@gmail.com>
2023-05-30 13:01:24 -0700

    fix type

--- components/dashboard/NewAnimalsChart.tsx
+++ components/dashboard/NewAnimalsChart.tsx
@@ -9,23 +9,9 @@ import {
   ResponsiveContainer
 } from 'recharts'
 import Title from './Title'
-import { listenAppUsers } from '@firebase/Users/main'
-import { User } from 'firebase/auth'
 import { myFormatDate } from 'utils/dates/myDateUtils'
 import { AnimalType } from 'types/base/AnimalType.model'
 import { listenAppAnimals } from '@firebase/Animal/main'
-
-// const data = [
-//   createData('00:00', 0),
-//   createData('03:00', 300),
-//   createData('06:00', 600),
-//   createData('09:00', 800),
-//   createData('12:00', 1500),
-//   createData('15:00', 2000),
-//   createData('18:00', 2400),
-//   createData('21:00', 2400),
-//   createData('24:00', undefined)
-// ]
 
 interface ChartData {
   date: string
@@ -61,7 +47,7 @@ export default function NewAnimalsChart() {
   const theme = useTheme()
   const [data, setData] = React.useState<ChartData[]>([])
   React.useEffect(() => {
-    listenAppAnimals((res: User[]) => {
+    listenAppAnimals((res: AnimalType[]) => {
       setData(newAnimalsToChartData(res))
     })
   }, [])
