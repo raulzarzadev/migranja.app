4caad014ed7a273355ac55b0a448109ad40a4db2
Raul Zarza <raulzarza.dev@gmail.com>
2023-06-05 21:54:58 -0700

    add positionfixed

--- components/UserHome/FarmMenu/index.tsx
+++ components/UserHome/FarmMenu/index.tsx
@@ -262,6 +262,7 @@ const FarmMenu = (props: any) => {
         <>
           <Fab
             onClick={modal.handleOpen}
+            sx={{ position: 'fixed' }}
             className="fixed bottom-4 right-4  bg-base-100"
           >
             <span className="">
